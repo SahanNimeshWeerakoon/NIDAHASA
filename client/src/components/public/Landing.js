5d5e2879ddc0b97c1831b72ed14446ad51a58ec0
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2020-09-08 03:41:50 +0530

    working on landing page. not fucking finished yet

--- client/src/components/public/Landing.js
+++ client/src/components/public/Landing.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import axios from 'axios'
 import { NavLink } from 'react-router-dom'
 import TopPlaces from '../modules/TopPlaces'
+import { FaHandPeace, FaDollarSign, FaHeartbeat } from 'react-icons/fa'
 
 const Landing = () => {
 
@@ -18,6 +19,47 @@ const Landing = () => {
 						<button>
 							<NavLink to="/allplaces" className="view-places">VIEW PLACES</NavLink>
 						</button>
+					</div>
+				</div>
+			</div>
+			<div className="benefits">
+				<h3 className="benefits-title">Benefits of NIDAHASA</h3>
+				<div className="row">
+					<div className="cell cell-4">
+						<div className="row">
+							<FaHandPeace className="ben-ico" />
+							<h5>Freedom</h5>
+						</div>
+						<small>
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
+							been the industry's standard dummy text ever since the 1500s, when an unknown printer took 
+							a galley of type and scrambled it to make a type specimen book. It has survived not only 
+							five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+						</small>
+					</div>
+					<div className="cell cell-4">
+						<div className="row">
+							<FaDollarSign className="ben-ico" />
+							<h5>Free of charge</h5>
+						</div>
+						<small>
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
+							been the industry's standard dummy text ever since the 1500s, when an unknown printer took 
+							a galley of type and scrambled it to make a type specimen book. It has survived not only 
+							five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+						</small>
+					</div>
+					<div className="cell cell-4">
+						<div className="row">
+							<FaHeartbeat className="ben-ico" />
+							<h5>I'm here for you</h5>
+						</div>
+						<small>
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
+							been the industry's standard dummy text ever since the 1500s, when an unknown printer took 
+							a galley of type and scrambled it to make a type specimen book. It has survived not only 
+							five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+						</small>
 					</div>
 				</div>
 			</div>
